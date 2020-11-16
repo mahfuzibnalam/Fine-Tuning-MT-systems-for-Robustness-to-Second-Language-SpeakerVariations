1d113bfe91798bc66e5c7614f2df0a2a5533228f
mahfuzibnalam <alam_mahfuz@yahoo.com>
2020-11-16 18:47:48 -0500

    Update finetune_optus-mt.sh

--- finetune_optus-mt.sh
+++ finetune_optus-mt.sh
@@ -6,14 +6,15 @@ export MAX_LEN=80
 python finetune.py \
     --learning_rate=3e-5 \
     --gpus 1 \
+    --do_train \
     --do_predict \
     --val_check_interval 0.2 \
     --adam_eps 1e-06 \
-    --num_train_epochs 1 \
+    --num_train_epochs 100 \
     --src_lang en_XX \
     --tgt_lang es \
     --data_dir $ENRO_DIR \
-    --output_dir=tests/verified_es_jfleg_original/all_corrected_fce_es \
+    --output_dir=tests/verified_es_jfleg_original/your_output \
     --max_source_length=80 \
     --max_target_length 80 \
     --val_max_target_length=80 \
@@ -21,7 +22,7 @@ python finetune.py \
     --train_batch_size=2 \
     --eval_batch_size=2 \
     --gradient_accumulation_steps=1 \
-    --model_name_or_path ~/transformers/examples/seq2seq/models/es/opus_mt_finetuned_all_corrected_fce/best_tfmr \
+    --model_name_or_path your_model \
     --task translation \
     --warmup_steps 500 \
     --sortish_sampler \
